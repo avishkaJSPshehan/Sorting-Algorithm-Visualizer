6fc08e319043dc17a8388827b6e112ecab06c185
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-06-19 02:39:44 +0530

    Add Quick Sort Function

---
+++ quickSort.py
@@ -0,0 +1,55 @@
+import time
+
+def partition(data, head, tail,drawData, timeTick):
+    border = head
+    pivot = data[tail]
+
+    drawData(data, colorArray(len(data),head,tail,border,border))
+    time.sleep(timeTick)
+
+    for j in range(head,tail):
+        if(data[j] < pivot):
+            drawData(data, colorArray(len(data),head,tail,border,j,True))
+            time.sleep(timeTick)
+            data[border],data[j] = data[j],data[border]
+            border += 1
+        drawData(data, colorArray(len(data),head,tail,border,j))
+        time.sleep(timeTick)
+
+
+    drawData(data, colorArray(len(data),head,tail,border,tail,True))
+    time.sleep(timeTick)
+    data[border],data[tail] = data[tail],data[border]
+    return border
+
+
+def quick_sort(data, head, tail,drawData, timeTick):
+    if head < tail:
+        partitionIdx = partition(data, head, tail,drawData,timeTick)
+
+
+        quick_sort(data, head, partitionIdx-1,drawData, timeTick) # LEFT partition
+        quick_sort(data, partitionIdx+1,tail ,drawData, timeTick) # RIGHT partition
+
+
+def colorArray(dataLen,head,tail,border,currIdex,isSwapping = False):
+    colorArray = []
+
+    for i in range(dataLen):
+        if i >= head and i <= tail:
+            colorArray.append("gray")
+        else:
+            colorArray.append("black")
+        
+        if i == tail:
+            colorArray[i] = "orange"
+        elif i == border:
+            colorArray[i] = "red"
+        elif i == currIdex:
+            colorArray[i] = "yellow"
+
+        if (isSwapping):
+            if i == border or i == currIdex:
+                colorArray[i] = "green"
+
+    return colorArray
